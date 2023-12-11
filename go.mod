d2f598b114da4a6491485e7219fd2ffb276f63c1
Jakub Hrozek <jakub@stacklok.com>
2023-12-11 16:33:44 +0100

    another main

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/jakubtestorg/bad-go
 
 go 1.20
-
-require golang.org/x/text v0.14.0
--- main.go
+++ main.go
@@ -2,35 +2,20 @@ package main
 
 import (
 	"fmt"
-
-	"golang.org/x/text/language"
-	"golang.org/x/text/message"
+	"html"
+	"log"
+	"net/http"
 )
 
 func main() {
-	// Register messages for different languages
-	message.SetString(language.English, "price", "The price is %v.")
-	message.SetString(language.Spanish, "price", "El precio es %v.")
-	message.SetString(language.German, "price", "Der Preis beträgt %v.")
+	// comment
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
 
-	// Define preferred languages
-	preferredLanguages := []language.Tag{
-		language.Spanish, // First preference
-		language.German,  // Second preference
-		language.English, // Third preference
-	}
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi There Yolanda!")
+	})
 
-	// Match preferred language
-	matcher := language.NewMatcher(preferredLanguages)
-	lang, _, _ := matcher.Match(language.Spanish, language.English)
-
-	// Print the price in the matched language
-	p := message.NewPrinter(lang)
-	price := 100.50
-	p.Printf("price", price)
-
-	// To demonstrate, let's print another message for English specifically
-	p = message.NewPrinter(language.English)
-	fmt.Println()
-	p.Printf("price", price)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
